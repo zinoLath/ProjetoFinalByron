51d7db0bb08969069008596648a2ff37c141b4e2
Zino Lath <zinolath@gmail.com>
2024-02-28 04:01:42 -0300

    header and footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,37 @@
+import instalogo from "@/assets/logo-insta.png"
+import logo from "@/assets/logo.png"
+import cards from "@/assets/card-companies.png"
+import Image from "next/image"
+
 export const Footer = () => {
   return (
-    <>
-      <h1>Footer</h1>
-    </>
+	<>
+	  <footer className="flex  bg-white min-h-96 h-fit" >
+		<div className="flex flex-col lg:flex-row items-center justify-between w-full py-10">
+			<div className="mx-auto lg:mx-0 lg:pl-40 lg:pr-0">
+				<div className="w-fit max-w-96 pb-5 px-5 text-justify">
+					Xerth Elojobs busca garantir que nossos serviços sejam executados com a máxima qualidade e segurança para nossos clientes,
+					uma vez que a sua satisfação é a nossa maior prioridade! Além disso, agilidade, profissionalismo e um preço super acesivel,
+					são algumas das vantages que o nosso serviço oferece!
+				</div>
+				<div className="">
+					<picture>
+						<Image src={instalogo} alt="instagram:" />
+					</picture>
+				</div>
+			</div>
+
+			<picture className="w-96 bg-red-400">
+				<Image src={logo} alt="Logo:" />
+			</picture>
+
+			<div className="lg:pr-40">
+				<picture className="w-96">
+					<Image src={cards} alt="Logo:" />
+				</picture>
+			</div>
+		</div>
+	  </footer>
+	</>
   );
 };
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,23 @@
+import logo from "@/assets/logo.png"
+import Image from "next/image"
+
 export const Header = () => {
   return (
-    <>
-      <h1>Header</h1>
-    </>
+	<>
+		<header className="flex items-center justify-center h-44 bg-white" >
+			<div className="flex flex-col lg:flex-row items-center justify-between w-full">
+				<picture className="pl-0 lg:pl-40">
+					<Image src={logo} alt="logo" className="h-28 w-48"/>
+				</picture>
+
+				<ul className="flex  items-center gap-10 pr-0 lg:pr-40 ">
+					<li>Home</li>
+					<li>Sobre</li>
+					<li>Elojob</li>
+					<li>Script</li>
+				</ul>
+			</div>
+		</header>
+	</>
   );
 };
